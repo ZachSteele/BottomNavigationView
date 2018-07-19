b843f87586efee256bdd49181634fb765e4d9215
ZachSteele <32414739+ZachSteele@users.noreply.github.com>
2018-07-19 11:04:57 -0400

    Small updates to naming convention.

--- app/src/main/java/com/myfirstproj/zachs/bottomnavigationview/MainActivity.java
+++ app/src/main/java/com/myfirstproj/zachs/bottomnavigationview/MainActivity.java
@@ -1,11 +1,9 @@
 package com.myfirstproj.zachs.bottomnavigationview;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatDialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
